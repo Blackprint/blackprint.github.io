c6d7c243952f61bf650a0afdee357bcb07c1bdf9
StefansArya <stefansarya1@gmail.com>
2020-03-07 10:59:04 +0700

    Improve performance on mobile device

---
+++ src/js/mobile.js
@@ -0,0 +1,21 @@
+var hasTouchScreen = false;
+if (navigator.maxTouchPoints !== void 0)
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+else if (navigator.msMaxTouchPoints !== void 0)
+    hasTouchScreen = navigator.msMaxTouchPoints > 0; 
+else {
+    var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+    if (mQ && mQ.media === "(pointer:coarse)")
+        hasTouchScreen = !!mQ.matches;
+    else {
+        // Fall back to user agent sniffing
+        var UA = navigator.userAgent;
+        hasTouchScreen = (
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+        );
+    }
+}
+
+if(hasTouchScreen)
+	$(document.body).addClass('disable-effect');
