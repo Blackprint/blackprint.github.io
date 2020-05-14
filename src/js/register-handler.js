7ae568f9d0fff2f3da803b77a98c87f791090bfd
StefansArya <stefansarya1@gmail.com>
2020-05-14 13:46:35 +0700

    Finishing example of node handler registration

--- src/js/register-handler.js
+++ src/js/register-handler.js
@@ -3,31 +3,41 @@ sketch.registerNode('math/multiply', function(handle, node){
 
 	node.title = "Multiply";
 	var inputs = handle.inputs = {
-		A:Number,
-		B:function(val){
-			console.log('node B got input:', val);
-			return Number(val);
+		Exec: function(){
+			handle.outputs.Result = multiply();
+			console.log("Result has been set:", handle.outputs.Result);
 		},
-		Finishz:Function
+		A: Number,
+		B: Blackprint.PortValidator(Number, function(val){
+			// Executed when inputs.B is being obtained
+			// And the output from other node is being assigned
+			// as current port value in this node
+			console.log(node.title, '- Port B got input:', val);
+			return Number(val);
+		}),
 	};
 
-	var outputs = handle.outputs = {
-		Result:Number,
-		Finish:Function,
-		Finishz:Function,
+	// Your own processing mechanism
+	function multiply(){
+		console.log('Multiplying', inputs.A, 'with', inputs.B);
+		return inputs.A * inputs.B;
+	}
+
+	handle.outputs = {
 		Result:Number,
 	};
 
+	// Event listener can only be registered after init
 	handle.init = function(){
-		node.on('cable.connect', function(){
-			console.log('ahoy');
+		node.on('cable.connect', function(cable){
+			console.log(`Cable connected from ${cable.owner.node.title} (${cable.owner.name}) to ${cable.target.node.title} (${cable.target.name})`);
 		});
 	}
 
-	// When node being executed
-	handle.run = function(){
-		console.log('processing', inputs.A, inputs.B);
-		outputs.Result = inputs.A * inputs.B;
+	// When any output value from other node are updated
+	// Let's immediately change current node result
+	handle.update = function(cable){
+		handle.outputs.Result = multiply();
 	}
 });
 
@@ -36,14 +46,29 @@ sketch.registerNode('math/random', function(handle, node){
 	node.description = "Number (0-100)";
 	node.dynamic = true; // Reexecute when doing loop somewhere
 
-	var outputs = handle.outputs = {
+	handle.outputs = {
 		Out:Number
 	};
 
-	// When node being executed
-	handle.run = function(){
-		console.log('processing', inputs.A, inputs.B);
-		outputs.Result = inputs.A * inputs.B;
+	var executed = false;
+	handle.inputs = {
+		'Re-seed':function(){
+			executed = true;
+			handle.outputs.Out = Math.round(Math.random()*100);
+		}
+	};
+
+	// When the connected node is requesting for the output value
+	handle.request = function(port, node){
+		// Only run once this node never been executed
+		// Return false if no value was changed
+		if(executed === true)
+			return false;
+
+		console.warn('Value request for port:', port.name, "from node:", node.title);
+
+		// Let's create the value for him
+		handle.inputs['Re-seed']();
 	}
 });
 
@@ -52,70 +77,94 @@ sketch.registerNode('display/logger', function(handle, node){
 	node.type = 'logger';
 	node.description = 'Print anything into text';
 	node.trigger = false;
-	handle.log = '';
 
-	var inputs = handle.inputs = {
-		Any:null
+	handle.inputs = {
+		Any: Blackprint.PortListener(function(port, val){
+			console.log("I connected to", port.name, "port from", port.node.title, "that have new value:", val);
+
+			// Let's take all data from all connected nodes
+			// Instead showing new single data-> val
+			refreshLogger(handle.inputs.Any);
+		})
 	};
 
-	// When node being executed
-	handle.run = function(){
-		var value = inputs.Any;
-
-		if(value === null)
-			handle.log = 'null';
-		else if(value === void 0)
-			handle.log = 'undefined';
-		else if(value.constructor === Function)
-			handle.log = value.toString();
-		else if(value.constructor === String || value.constructor === Number)
-			handle.log = value;
+	function refreshLogger(val){
+		if(val === null)
+			node.log = 'null';
+		else if(val === void 0)
+			node.log = 'undefined';
+		else if(val.constructor === Function)
+			node.log = val.toString();
+		else if(val.constructor === String || val.constructor === Number)
+			node.log = val;
 		else
-			handle.log = JSON.stringify(value);
+			node.log = JSON.stringify(val);
+	}
+
+	handle.init = function(){
+		// Let's show data after new cable was connected or disconnected
+		node.on('cable.connect cable.disconnect', function(){
+			console.log("A cable was changed on Logger, now refresing the input element");
+			refreshLogger(handle.inputs.Any);
+		});
 	}
 });
 
-sketch.registerNode('button/test', function(handle, node){
+sketch.registerNode('button/simple', function(handle, node){
+	// node = under ScarletsFrame element control
 	node.title = "Button";
 	node.type = 'button';
 
-	var outputs = handle.outputs = {
-		Clicked:Object
+	// handle = under Blackprint node flow control
+	handle.outputs = {
+		Clicked:Function
 	};
-console.log(handle, node);
-	handle.run = function(){
-		console.log('hey', arguments);
+
+	// Proxy event object from: node.clicked -> handle.clicked -> outputs.Clicked
+	handle.clicked = function(ev){
+		console.log('button/simple: got', ev, "time to trigger to the other node");
+		handle.outputs.Clicked(ev);
 	}
 });
 
-sketch.registerNode('input/test', function(handle, node){
+sketch.registerNode('input/simple', function(handle, node){
+	// node = under ScarletsFrame element control
 	node.title = "Input";
 	node.type = 'input';
 
-	var outputs = handle.outputs = {
-		Value:Object
+	// handle = under Blackprint node flow control
+	handle.outputs = {
+		Changed:Function,
+		Value:'', // Default to empty string
 	};
 
-	handle.run = function(){
-		console.log('hey', arguments);
+	// Proxy string value from: node.changed -> handle.changed -> outputs.Value
+	// And also call outputs.Changed() if connected to other node
+	handle.changed = function(text, ev){
+		console.log('The input box have new value:', text);
+		handle.outputs.Value = text;
+
+		// This will call every connected node
+		handle.outputs.Changed();
 	}
 });
 
+// Does nothing :3
 sketch.registerNode('dummy/test', function(handle, node){
-	node.title = "Test 1";
+	node.title = "Do nothing";
 
 	// PortName must different any port
-	var inputs = handle.inputs = {
+	handle.inputs = {
 		"Input 1":Boolean,
 		"Input 2":String
 	};
 
-	var outputs = handle.outputs = {
+	handle.outputs = {
 		"Output 1":Object,
 		"Output 2":Number
 	};
 
-	var properties = handle.properties = {
+	handle.properties = {
 		"Property 1":Boolean,
 		"Property 2":Number
 	};
