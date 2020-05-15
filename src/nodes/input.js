9e20a510d55442e107aeb7c9342c4dee4f46a30a
StefansArya <stefansarya1@gmail.com>
2020-05-15 08:24:38 +0700

    Improve the textarea to support auto scaling

--- src/nodes/input.js
+++ src/nodes/input.js
@@ -13,7 +13,30 @@ sketch.registerInterface('input', {
 		properties: [],
 	} */
 
-	self.log = '';
+	// Must be placed on 'options' if we want to export the properties as JSON
+	self.options = {
+		// Two way binding-> https://github.com/ScarletsFiction/ScarletsFrame/wiki/Input-Binding
+		value:'',
+
+		// Listener for two way binding when value from HTML input element is being received by the framework
+		// I'm using this for auto scale the width/height of the textarea
+		v2m$value:function(old, now){
+			// Scale the input box depend on character length
+			var el = self.$el('textarea');
+
+			// Skip if textarea was larger than our auto control
+			// I mean, if user have change the size manually
+			if(el[0].offsetWidth > 150 || el[0].offsetHeight > 60)
+				return;
+
+			if(now.length < 8)
+				el.attr('style', '');
+			else if(now.length >= 8 && now.length < 14)
+				el.attr('style', 'width:'+(10*now.length)+'px');
+			else if(now.length >= 14)
+				el.attr('style', 'width:140px;height:50px');
+		}
+	};
 
 	self.textChanged = function(ev){
 		self.handle.changed(ev.target.value);
@@ -33,13 +56,15 @@ sketch.registerInterface('input', {
 // You must design this to support non-browser JavaScript
 // As you can just copy/import this into Node.js or Deno script
 interTest.registerInterface('input', function(self, bind){
-	var theLog = '...';
+	var theValue = '...';
 	bind({
-		set log(val){
-			theLog = val;
-		},
-		get log(){
-			return theLog;
+		options:{
+			set value(val){
+				theValue = val;
+			},
+			get value(){
+				return theValue;
+			}
 		}
 	});
 
--- src/nodes/logger.js
+++ src/nodes/logger.js
@@ -13,8 +13,27 @@ sketch.registerInterface('logger', {
 		properties: [],
 	} */
 
-	// Input binding-> https://github.com/ScarletsFiction/ScarletsFrame/wiki/Input-Binding
+	// One way binding-> https://github.com/ScarletsFiction/ScarletsFrame/wiki/Input-Binding
 	self.log = '...';
+
+	// Listener when log value is being send to HTML input element by the framework
+	// I'm using this for auto scale the width/height of the textarea
+	self.m2v$log = function(old, now){
+		// Scale the input box depend on character length
+		var el = self.$el('textarea');
+
+		// Skip if textarea was larger than our auto control
+		// I mean, if user have change the size manually
+		if(el[0].offsetWidth > 150 || el[0].offsetHeight > 60)
+			return;
+
+		if(now.length < 8)
+			el.attr('style', '');
+		else if(now.length >= 8 && now.length < 14)
+			el.attr('style', 'width:'+(10*now.length)+'px');
+		else if(now.length >= 14)
+			el.attr('style', 'width:140px;height:50px');
+	}
 });
 
 
