3b43f293edaf9253a099f4a48e7e584f9f96ba7b
StefansArya <stefansarya1@gmail.com>
2021-08-09 13:51:20 +0700

    Add dropdown component

--- src/model/header.js
+++ src/model/header.js
@@ -8,8 +8,103 @@ sf.model('header', function(My, include){
 		sf.URI.parse(); // Trigger to reparse current URL
 	}
 
-	My.mainMenu = function(){
+	My.mainMenu = function(ev){
+		if(My.showOptions === false) return;
 
+		include('dropdown').show([{
+			title: 'Sketch',
+			icon: 'fa fa-pencil-ruler',
+			deep:[{
+				title: 'Import',
+				icon: 'fa fa-folder-open',
+				deep:[{
+					title:'Append JSON',
+					async callback(){
+						sketch.importJSON(await swal({
+							title:"Import from JSON and append to sketch",
+							content: "input",
+						}));
+					}
+				}, {
+					title: 'Load JSON',
+					async callback(){
+						let val = await swal({
+							title:"Clean sketch and import from JSON",
+							content: "input",
+						});
+
+						if(!val) return;
+
+						sketch.clearNodes();
+						sketch.importJSON(val);
+					}
+				}, {
+					title: 'Load File',
+					callback(){
+						var el = document.createElement("input");
+						el.setAttribute('type', 'file');
+						el.onchange = function(){
+							console.log(345, this);
+						}
+						el.click();
+					}
+				}]
+			}, {
+				title:'Export',
+				icon: 'fa fa-save',
+				deep:[{
+					title: 'Copy JSON',
+					callback(){
+						var temp = sketch.exportJSON();
+						navigator.clipboard.writeText(temp);
+
+						swal({
+							title: "Copied to clipboard!",
+							text: temp
+						});
+					}
+				}, {
+					title: 'To File',
+					callback(){
+						var btn = document.createElement("a");
+						var file = new Blob([sketch.exportJSON()], {type: 'application/json'});
+						btn.href = URL.createObjectURL(file);
+						btn.download = 'blackprint.json';
+						btn.click();
+
+						// Auto revoke after 120 sec
+						setTimeout(function(){
+							URL.revokeObjectURL(btn.href);
+						}, 120e3);
+					}
+				}]
+			}]
+		}, {
+			title: 'Modules',
+			icon: 'fa fa-layer-group',
+			deep:[{
+				title: 'Create Custom',
+				icon: 'fa fa-plus',
+				callback(){}
+			}, {
+				title: 'Load from URL',
+				icon: 'fa fa-truck-loading',
+				callback(){}
+			}]
+		}, {
+			title: 'Environment',
+			icon: 'fa fa-key',
+			callback(){}
+		}, {
+			title: 'Home',
+			icon: 'fa fa-home',
+			callback(){
+				views.goto('/')
+			}
+		}], {
+			element: My.$el('.header-left')[0],
+			className: 'header-left-menu'
+		});
 	}
 
 	My.cloneActive = false;
@@ -35,72 +130,5 @@ sf.model('header', function(My, include){
 	My.visualizeFlow = function(){
 		My.visualizeActive = !My.visualizeActive;
 		Blackprint.settings('visualizeFlow', My.visualizeActive);
-	}
-
-	My.saveSketch = function(ev){
-		sketch.scope('dropdown').show([{
-			title:'Copy JSON',
-			callback(){
-				var temp = sketch.exportJSON();
-				navigator.clipboard.writeText(temp);
-
-				swal({
-					title: "Copied to clipboard!",
-					text: temp
-				});
-			}
-		}, {
-			title:'To File',
-			callback(){
-				var btn = document.createElement("a");
-				var file = new Blob([sketch.exportJSON()], {type: 'application/json'});
-				btn.href = URL.createObjectURL(file);
-				btn.download = 'blackprint.json';
-				btn.click();
-
-				// Auto revoke after 120 sec
-				setTimeout(function(){
-					URL.revokeObjectURL(btn.href);
-				}, 120e3);
-			}
-		}], ev.x, ev.y);
-	}
-
-	My.loadSketch = function(ev){
-		sketch.scope('dropdown').show([{
-			title:'Append from JSON',
-			callback(){
-				swal({
-					title:"Append from JSON",
-					content: "input",
-				}).then(function(val){
-					sketch.importJSON(val);
-				});
-			}
-		}, {
-			title:'Import JSON',
-			callback(){
-				swal({
-					title:"Import JSON",
-					content: "input",
-				}).then(function(val){
-					if(!val)
-						return;
-
-					sketch.clearNodes();
-					sketch.importJSON(val);
-				});
-			}
-		}, {
-			title:'From File',
-			callback(){
-				var el = document.createElement("input");
-				el.setAttribute('type', 'file');
-				el.onchange = function(){
-					console.log(345, this);
-				}
-				el.click();
-			}
-		}], ev.x, ev.y);
 	}
 });
