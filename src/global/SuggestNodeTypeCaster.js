5e43c458bf07cf7b17c9805cf0b1eb051d946b94
StefansArya <stefansarya1@gmail.com>
2022-03-14 08:10:08 +0700

    Add auto type caster node

---
+++ src/global/SuggestNodeTypeCaster.js
@@ -0,0 +1,97 @@
+$(()=>{
+	setTimeout(async ()=> {
+		await sf.loader.task;
+
+		if(Blackprint.nodes.Data != null)
+			return;
+
+		SmallNotif.add("Loading required nodes", 'yellow', 500);
+
+		// Load nodes for data manipulation
+		Blackprint.loadModuleFromURL('https://cdn.jsdelivr.net/npm/@blackprint/nodes@0.4/dist/nodes-data.mjs');
+	}, 3000);
+});
+
+function SuggestNodeTypeCaster(ev){
+	if(Blackprint.nodes.Data == null)
+		return;
+
+	let { cable, port, target, handler } = ev;
+
+	let input, output;
+	if(cable.owner.source === 'input'){
+		input = port;
+		output = target;
+	}
+	else {
+		input = target;
+		output = port;
+	}
+
+	if(output.type === Number && input.type === String){
+		handler(() => {
+			let iface = SuggestNodeTypeCaster.createNode('Data/Number/To/String', ev, input);
+
+			if(cable.owner === output){
+				iface.input.In.connectCable(cable);
+				iface.output.Out.connectPort(input);
+			}
+			else {
+				iface.input.In.connectPort(output);
+				iface.output.Out.connectCable(cable);
+			}
+		});
+	}
+	else if(output.type === String && input.type === Number){
+		handler(() => {
+			let iface = SuggestNodeTypeCaster.createNode('Data/String/To/Number', ev, input);
+
+			if(cable.owner === output){
+				iface.input.In.connectCable(cable);
+				iface.output.Out.connectPort(input);
+			}
+			else {
+				iface.input.In.connectPort(output);
+				iface.output.Out.connectCable(cable);
+			}
+		});
+	}
+	else if(!SuggestNodeTypeCaster.primitive.has(output.type) && SuggestNodeTypeCaster.primitive.has(input.type)){
+		handler(() => {
+			let iface = SuggestNodeTypeCaster.createNode('Data/Object/GetValue', ev, input);
+
+			if(cable.owner === output){
+				iface.input.Object.connectCable(cable);
+				iface.output.Value.connectPort(input);
+			}
+			else {
+				iface.input.Object.connectPort(output);
+				iface.output.Value.connectCable(cable);
+			}
+		});
+	}
+}
+
+SuggestNodeTypeCaster.primitive = new Set([String, Number, Boolean, BigInt]);
+SuggestNodeTypeCaster.createNode = function(namespace, ev, input){
+	let instance =  ev.instance;
+	let rect = input.iface.input._list.getElement(input).getBoundingClientRect();
+	let container = instance.scope('container');
+
+	let Ofst = container.offset;
+	let y = (rect.y - container.pos.y - Ofst.y) / container.scale
+	let x = (rect.x - container.pos.x - Ofst.x) / container.scale
+	let iface = instance.createNode(namespace, {x, y});
+
+	setTimeout(()=> {
+		if(iface.w == null) return;
+		iface.moveNode({
+			stopPropagation(){}, preventDefault(){},
+			target: iface.$el[0],
+			movementX: -(iface.w + 20) * container.scale,
+			movementY: -2
+		});
+	}, 300);
+
+	return iface;
+}
