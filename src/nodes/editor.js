d4505a1e19d6dea45acc1d39943327abb86bfb2f
StefansArya <stefansarya1@gmail.com>
2022-03-28 19:27:32 +0700

    Add editor nodes for previewing a node

---
+++ src/nodes/editor.js
@@ -0,0 +1,21 @@
+Blackprint.registerNode('BP_Editor/LeftPanel/Preview',
+class extends Blackprint.Node {
+	static input = { Element: HTMLElement };
+	constructor(instance){
+		super(instance);
+
+		let iface = this.setInterface();
+		iface.title = "Editor Preview"
+		iface.description = "Put element on the left panel";
+
+		// Manually call 'update' when any cable from input port was disconnected
+		this.iface.on('cable.disconnect', {slot: 'bp-editor'}, ({ port })=> {
+			if(port.source === 'input') this.update();
+		});
+	}
+
+	update(){
+		let { page } = this._instance;
+		page.panels.left.setPreview(this.input.Element);
+	}
+});
