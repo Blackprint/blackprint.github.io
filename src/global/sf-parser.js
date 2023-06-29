a1747bf7f836d576c40cad9d05396b160e25a62e
StefansArya <stefansarya1@gmail.com>
2023-06-29 13:00:55 +0700

    Add notification if some feature is not available

--- src/global/sf-parser.js
+++ src/global/sf-parser.js
@@ -1,5 +1,5 @@
 // This only available on development mode editor (as this have eval, it can be dangerous)
-if(sf.hotReload){
+if(sf.hotReload != null && location.pathname === '/dev.html'){
 	sf.loader.js(['https://cdn.jsdelivr.net/npm/sass.js@0.11.1/dist/sass.sync.js']);
 	sf.parser = {
 		parseTemplate(text){
